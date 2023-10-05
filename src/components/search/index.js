d94caa61d3d80a8c5e3fac3b838188ae4d506798
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-04 19:05:17 -0700

    start working on graph

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,21 +1,80 @@
-import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Colors from "../../style/colors";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
-export default class SearchScreen extends Component{
-render(){
-    return(
-        <SafeAreaView style={styles.container}>
-            <Text>Search</Text>
-        </SafeAreaView>
-    )
-}
+export default class SearchScreen extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state
+    this.state = {
+      isButtonDisabled: false,
+    };
+  }
+  handleButtonClick = () => {
+    this.setState({ isButtonDisabled: true });
+  };
+  render() {
+    let {isButtonDisabled}=this.state
+    return (
+      <View style={styles.container}>
+        {!isButtonDisabled && (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.handleButtonClick}
+          >
+            <View style={{flexDirection:'row'}}>
+                <MaterialCommunityIcons name="graphql" size={24} color={Colors.white}  style={{marginRight:10}}/>
+                <Text style={styles.buttonText}>Simulation</Text>
+            </View>
+        
+          </TouchableOpacity>
+        )}
+         {isButtonDisabled && (
+          <TouchableOpacity
+            style={styles.disableBtn}
+          >
+             <View style={{flexDirection:'row'}}>
+                <MaterialCommunityIcons name="graphql" size={24} color={Colors.white} style={{marginRight:10}}/>
+                <Text style={styles.buttonText}>Simulation</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  button: {
+    backgroundColor: Colors.orange,
+    padding: 8,
+    margin: 5,
+    borderRadius: 5,
+    height: 35,
+    width: "20%",
+  },
+  disableBtn:{
+    backgroundColor: Colors.grey,
+    padding: 8,
+    margin: 5,
+    borderRadius: 5,
+    height: 35,
+    width: "20%",
+  },
+  buttonText: {
+    color: Colors.white,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
 });
