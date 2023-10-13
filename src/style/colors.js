1ca611a894bc690d10001199ef31a3526f1274f9
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-13 15:36:28 -0700

    manual changes

--- src/style/colors.js
+++ src/style/colors.js
@@ -2,12 +2,9 @@ const Colors = {
     white: '#ffffff',
     error: 'red',
     black: '#000000',
-    // orange:'#FF5733',
-    // orange:'#FF5F1F',
     orange:'#FF7518',
     lightOrange:'#FFB38A',
     grey:'#B8B8B8',
-    info:'#87CEFA'
   };
   
   export default Colors;
